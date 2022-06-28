e28679db2f81565467858a346977adde7a589369
kor-Chipmunk <rhj4862@gmail.com>
2022-06-29 03:35:53 +0900

    feat(QRCode): react-qr-code 패키지 설치 & 예시 컴포넌트 작성
    ISSUES CLOSED: #4

--- pages/rooms/[roomId]/qr/index.tsx
+++ pages/rooms/[roomId]/qr/index.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import type { NextPage } from "next";
+import styled from "@emotion/styled";
+import QRCode from "react-qr-code";
 
 const RoomQRPage: NextPage = () => {
-  return <div>RoomQRPage</div>;
+  return (
+    <div>
+      <p>아래 QR 이미지를 공유해보세요.</p>
+      <QRCodeContainer data-testid="QRCodeContainer">
+        <QRCode value="https://musictogether.netlify.app/" size={128} />
+      </QRCodeContainer>
+    </div>
+  );
 };
+
+const QRCodeContainer = styled.div`
+  background: white;
+  padding: 16px;
+`;
 
 export default RoomQRPage;
