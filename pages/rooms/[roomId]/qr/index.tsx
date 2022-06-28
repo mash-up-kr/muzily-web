d02a4e6c369ff1249a462f00693ae56f17efd447
kor-Chipmunk <rhj4862@gmail.com>
2022-06-29 03:43:16 +0900

    fix(index.tsx): 테스트 태그 제거
    ISSUES CLOSED: #4

--- pages/rooms/[roomId]/qr/index.tsx
+++ pages/rooms/[roomId]/qr/index.tsx
@@ -7,7 +7,7 @@ const RoomQRPage: NextPage = () => {
   return (
     <div>
       <p>아래 QR 이미지를 공유해보세요.</p>
-      <QRCodeContainer data-testid="QRCodeContainer">
+      <QRCodeContainer>
         <QRCode value="https://musictogether.netlify.app/" size={128} />
       </QRCodeContainer>
     </div>
