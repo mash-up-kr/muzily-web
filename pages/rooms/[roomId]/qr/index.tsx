0183a019086adec2ad3dbe2365314215d0687be9
kor-Chipmunk <rhj4862@gmail.com>
2022-06-30 07:42:44 +0900

    test(RoomQRPage): data-testid 추가 & QR 코드 주변 흰 배경 있는지, QR 코드가 나타나는지 테스트 케이스 추가

--- pages/rooms/[roomId]/qr/index.tsx
+++ pages/rooms/[roomId]/qr/index.tsx
@@ -7,8 +7,12 @@ const RoomQRPage: NextPage = () => {
   return (
     <div>
       <p>아래 QR 이미지를 공유해보세요.</p>
-      <QRCodeContainer>
-        <QRCode value="https://musictogether.netlify.app/" size={128} />
+      <QRCodeContainer data-testid="QRCodeContainer">
+        <QRCode
+          data-testid="QRCode"
+          value="https://musictogether.netlify.app/"
+          size={128}
+        />
       </QRCodeContainer>
     </div>
   );
