9a86306a06902b2e6db2bf84b366c80c0f96d33f
kor-Chipmunk <rhj4862@gmail.com>
2022-07-10 00:52:12 +0900

    feat(QRCodeWithLogo): url props 추가 & 매직넘버 상수 처리
    ISSUES CLOSED: #30

--- components/uis/QRCodeWithLogo/index.tsx
+++ components/uis/QRCodeWithLogo/index.tsx
@@ -1,27 +1,35 @@
 import styled from "@emotion/styled";
 import { QRCode } from "react-qrcode-logo";
 
+interface QRCodeWithLogoProps {
+  url: string;
+}
+
 type QRStyleType = "dots" | "squares";
 
+const logoStaticImage = "/images/qr-code-logo.png";
 const logoSize = {
   width: 37,
   height: 40,
 };
 const eyeRadius = 6;
-const qrSize = 198;
+const qrSize = 178;
 const qrStyle: QRStyleType = "dots";
+const qrCodeMarginAreaSize = 20;
 
-const QRCodeWithLogo = () => {
+const QRCodeWithLogo = (props: QRCodeWithLogoProps) => {
   return (
     <QRCodeContainer>
       <QRCode
-        value="http://localhost:3000/rooms/1/"
-        logoImage="/images/qr-logo.ico"
+        value={props.url}
+        logoImage={logoStaticImage}
         logoWidth={logoSize.width}
         logoHeight={logoSize.height}
         eyeRadius={eyeRadius}
         size={qrSize}
         qrStyle={qrStyle}
+        removeQrCodeBehindLogo={true}
+        quietZone={qrCodeMarginAreaSize}
       />
     </QRCodeContainer>
   );
