8cbf1b9d4e565ec1ea8c8a03e3c817b4c92ace80
jiyoung.lim <swimjiy@gmail.com>
2022-07-10 03:10:56 +0900

    feat(create room): add topbar to create room
    ISSUES CLOSED: #5
    ISSUES CLOSED: #3
    ISSUES CLOSED: #3
    ISSUES CLOSED:
    #29
    ISSUES
    CLOSED:
    ISSUES
    CLOSED: #29
    ISSUES CLOSED: #29
    ISSUES CLOSED: #29
    ISSUES CLOSED: #29
    
    ISSUES CLOSED: #29

--- pages/rooms/create/index.tsx
+++ pages/rooms/create/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import type { NextPage } from "next";
+import { Layout, TopBar, TopBarIconButton } from "~/components/uis";
 
 const RoomCreatePage: NextPage = () => {
-  return <div>RoomCreatePage</div>;
+  return (
+    <Layout>
+      <TopBar leftIconButton={<TopBarIconButton iconName="arrow-left" />}>
+        방 만들기
+      </TopBar>
+    </Layout>
+  );
 };
 
 export default RoomCreatePage;
