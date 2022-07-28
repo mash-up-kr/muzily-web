6a744d148c5fe98d02ad99e96780d22d67d9b528
kor-Chipmunk <rhj4862@gmail.com>
2022-07-28 22:34:27 +0900

    feat(custom): 메인 인트로 페이지 UI 작성

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,18 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import { Layout, TopBar, TopBarIconButton } from "~/components/uis";
-import { StyledTopBarRightItem, StyledInviteContainer } from "./styled";
+import { withRouteGuard } from "~/hocs";
+import {
+  StyledTopBarRightItem,
+  StyledInviteContainer,
+  StyeldForm,
+  StyledRoomTextContainer,
+  StyledRoomInputText,
+  StyledSpacer,
+  StyledHeader,
+  StyledCreateRoomButton,
+  StyledDescripion,
+} from "./styled";
 
-const SignupPage: NextPage = () => {
-  return (
-    <Layout screenColor="black">
-      <TopBar
-        leftIconButton={<TopBarIconButton iconName="star" />}
-        rightIconButton={<StyledTopBarRightItem>로그인</StyledTopBarRightItem>}
-      />
-      <StyledInviteContainer></StyledInviteContainer>
-    </Layout>
-  );
-};
+const SignupPage: NextPage = withRouteGuard(
+  { UNCONNECTED: true },
+  "/login",
+  () => {
+    const [roomTitle, setRoomTitle] = useState("");
+    const router = useRouter();
+
+    const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      router.push({
+        pathname: "/rooms/create",
+        query: { roomTitle },
+      });
+    };
+
+    return (
+      <Layout screenColor="black">
+        <TopBar
+          leftIconButton={<TopBarIconButton iconName="star" />}
+          rightIconButton={
+            <StyledTopBarRightItem>로그인</StyledTopBarRightItem>
+          }
+        />
+        <StyeldForm onSubmit={onSubmit}>
+          <StyledInviteContainer>
+            <StyledRoomTextContainer>
+              <StyledRoomInputText
+                type="text"
+                onChange={(e) => setRoomTitle(e.target.value)}
+              ></StyledRoomInputText>
+            </StyledRoomTextContainer>
+            <StyledSpacer></StyledSpacer>
+            <StyledHeader>
+              함께 만드는
+              <br />
+              모두의 플레이리스트
+            </StyledHeader>
+            <StyledCreateRoomButton type="submit">
+              방 만들기
+            </StyledCreateRoomButton>
+            <StyledDescripion>지금 바로 3초만에 만들어보세요!</StyledDescripion>
+          </StyledInviteContainer>
+        </StyeldForm>
+      </Layout>
+    );
+  }
+);
 
 export default SignupPage;
--- pages/main/styled.tsx
+++ pages/main/styled.tsx
@@ -16,6 +16,78 @@ export const StyledInviteContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  padding: 0 60px;
   gap: 6px;
-  height: 80%;
+`;
+
+export const StyeldForm = styled.form`
+  width: 100%;
+  max-width: 100vw;
+  min-height: 100vh;
+  height: 100vh;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-top: 48px;
+  padding-bottom: constant(safe-area-inset-bottom);
+  padding-bottom: env(safe-area-inset-bottom);
+`;
+
+export const StyledRoomTextContainer = styled.div`
+  width: 100%;
+  height: 45px;
+  padding: 4px 8px;
+  background-color: #333;
+  border-radius: 7px;
+`;
+
+export const StyledRoomInputText = styled.input`
+  width: 100%;
+  height: 100%;
+  background-color: #333;
+  border: none;
+  color: white;
+  font-size: 26px;
+`;
+
+export const StyledSpacer = styled.div`
+  width: 100%;
+  height: 2px;
+`;
+
+export const StyledHeader = styled.div`
+  font: "Apple SD Gothic Neo";
+  font-weight: 700;
+  font-size: 26px;
+  color: white;
+  text-align: center;
+  line-height: 43.2px;
+`;
+
+export const StyledCreateRoomButton = styled.button`
+  margin-top: 58px;
+  width: 149px;
+  height: 51px;
+  background-color: #007aff;
+  border-radius: 15px;
+  color: white;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 23.87px;
+  letter-spacing: -0.45px;
+  text-align: center;
+  border: none;
+  cursor: pointer;
+`;
+
+export const StyledDescripion = styled.span`
+  margin-top: 18px;
+  font-size: 14px;
+  line-height: 22.4px;
+  color: #d9d9d9;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 500;
 `;
