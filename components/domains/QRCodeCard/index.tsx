125dcf35bcb65cc92f03f806e2337659fab916ac
jiyoung.lim <swimjiy@gmail.com>
2022-08-21 01:00:23 +0900

    feat(qrcode): qR code url 적용
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    ISSUES
    CLOSED:
    #105
    ISSUES CLOSED: #116

--- components/domains/QRCodeCard/index.tsx
+++ components/domains/QRCodeCard/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import { QRCodeWithLogo } from "~/components/uis";
+import { useRoomQuery } from "~/hooks/api";
 import WebShareButton from "../WebShareButton";
 
 interface QRCodeCardProps {
@@ -8,27 +9,31 @@ interface QRCodeCardProps {
 }
 
 function QRCodeCard({ roomId }: QRCodeCardProps) {
+  const [inviteURL, setInviteURL] = useState("");
+  const { data } = useRoomQuery(Number(roomId));
   const [disabled, setDisabled] = useState(false);
 
   useEffect(() => {
     const qrcode: HTMLCanvasElement | null =
       document.querySelector("#react-qrcode-logo");
-
     setDisabled(qrcode === null);
-  });
+  }, []);
+
+  useEffect(() => {
+    if (data !== undefined) {
+      setInviteURL(
+        `${window.location.protocol}//${window.location.host}/rooms/${roomId}/invite?inviteKey=${data.invitation.invitationKey}`
+      );
+    }
+  }, [data]);
 
   return (
     <S.Container>
       <S.UpContainer>
         <S.Title>QR Code</S.Title>
-        <QRCodeWithLogo
-          url={`https://developmusily.netlify.app/rooms/${roomId}`}
-        ></QRCodeWithLogo>
+        <QRCodeWithLogo url={inviteURL}></QRCodeWithLogo>
       </S.UpContainer>
-      <WebShareButton
-        shareURL={`https://developmusily.netlify.app/rooms/${roomId}`}
-        disabled={disabled}
-      />
+      <WebShareButton shareURL={inviteURL} disabled={disabled} />
     </S.Container>
   );
 }
